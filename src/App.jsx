be6467fb862a58d83e8c959a8577818ae10cf1d3
triptishakya-dev <triptishakya.2001@gmail.com>
2024-09-30 10:26:24 +0530

    completed structure of the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
-
+import { Navbar } from './component/navbar'
+import HeroSection from './component/HeroSection'
+import BlogSection from './component/BlogSection'
+import Footer from './component/Footer'
 const App = () => {
   return (
     <div>
-      <h1>
-        blog-website
-      </h1>
+     <Navbar/>
+     <HeroSection/>
+     <BlogSection/>
+     <Footer/>
     </div>
   )
 }
---
+++ src/component/HeroSection.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+        <h1>
+            my hero section
+        </h1>
+        </div>
+  )
+}
+
+export default HeroSection
